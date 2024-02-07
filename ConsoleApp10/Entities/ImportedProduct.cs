647b1c8f5ca6388e714721c908fe131b29f47a14
Victor R. Silva <victorr.silva005@gmail.com>
2024-02-07 19:16:45 -0300

    Created classes and methods

---
+++ ConsoleApp10/Entities/ImportedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override sealed string PriceTag()
+        {
+            return base.PriceTag();
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+    }
+}
---
+++ ConsoleApp10/Entities/UsedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufacturerDate { get; set; }
+        
+        public UsedProduct() { }
+
+        public UsedProduct(DateTime manufacturerDate)
+        {
+            ManufacturerDate = manufacturerDate;
+        }
+
+        public override sealed string PriceTag()
+        {
+            return base.PriceTag();
+        }
+    }
+}
